98bcd46279fb9aadf7f7dde3f9a14a0048f3560c
Ricardo Magalhães <ricardospilotes@gmail.com>
2023-12-13 00:52:23 -0300

    Update scrollreveal.js

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -2,7 +2,7 @@ ScrollReveal({ reset: true });
 
 ScrollReveal().reveal(".salutation", {
   duration: 1000,
-  delay: 4500,
+  delay: 2500,
 });
 
 ScrollReveal().reveal(".logo", {
@@ -12,5 +12,5 @@ ScrollReveal().reveal(".logo", {
 
 ScrollReveal().reveal(".ano", {
   duration: 1000,
-  delay: 4500,
+  delay: 2500,
 });
