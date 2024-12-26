4cb1aa8e579fbf9d3838211f02ee1e22b4424dbe
Ricardo Magalhães <ricardospilotes@gmail.com>
2024-12-26 01:48:16 -0300

    Add files via upload

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -10,11 +10,6 @@ ScrollReveal().reveal(".logo1", {
   delay: 2500,
 });
 
-ScrollReveal().reveal(".links-container", {
-  duration: 1000,
-  delay: 2500,
-});
-
 ScrollReveal().reveal(".ano", {
   duration: 1000,
   delay: 2500,
