9991df72ceeda158dcfb010b2a62fe82543470a6
Ricardo Magalhães <ricardospilotes@gmail.com>
2025-10-08 13:50:15 -0300

    Add files via upload

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -2,15 +2,40 @@ ScrollReveal({ reset: true });
 
 ScrollReveal().reveal(".salutation", {
   duration: 1000,
-  delay: 2500,
+  delay: 2000,
 });
 
-ScrollReveal().reveal(".logo1", {
+ScrollReveal().reveal(".name", {
   duration: 1000,
-  delay: 2500,
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".mail", {
+  duration: 1000,
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".links-title", {
+  duration: 1000,
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".bbc-img", {
+  duration: 1000,
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".bbc-link", {
+  duration: 1000,
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".links-container", {
+  duration: 1000,
+  delay: 2000,
 });
 
 ScrollReveal().reveal(".ano", {
   duration: 1000,
-  delay: 2500,
+  delay: 2000,
 });
